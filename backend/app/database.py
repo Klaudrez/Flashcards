6f975237ab955a3fff7963d831a61ee6d46a1f38
Andres <andres.claussen.eng@gmail.com>
2025-10-13 21:53:47 -0300

    Connecting Database Postgres

---
+++ backend/app/database.py
@@ -0,0 +1,27 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+connection_url = URL.create(
+    drivername="postgresql",
+    username=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST", "localhost"),
+    port=os.getenv("POSTGRES_PORT", 5432),
+    database=os.getenv("POSTGRES_DB")
+)
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+
+
+engine = create_engine(connection_url)
+
+Session = sessionmaker(bind=engine)
+session = Session()
